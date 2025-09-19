8a8c7b4f9e200d8ea29257810f2588bb18cd7b6b
Sanvitti <86451908+ndmickma@users.noreply.github.com>
2025-09-18 19:36:03 -0700

    Add files via upload

---
+++ SummerVacation.java
@@ -0,0 +1,28 @@
+//Sanvitti Shah
+//8-26-25 Per. 2
+//SummerVacation.java
+/*Description of program
+ * list out what you did over the summer
+ * Working on:
+ * using print statements println, print
+ * using escape sequences for tab \t and new line \n
+ */
+ 
+ public class SummerVacation
+ {
+	 public static void main (String [] args)
+	 {
+		 System.out.println("\n\n\n");
+		 System.out.println("My \"Summer Vacation\":");
+		 System.out.print("This summer ");
+		 System.out.print("I did many things like:");
+		 System.out.println("\n\t Go to Seattle and Canada!");
+		 System.out.print("\t After that, on July 8th, "+
+			"I went to the Weeknd concert!");
+		 System.out.print("\n\t I started volunteering at summer camps! ");
+		 System.out.println("\n Overall, I had a lot of fun this summer!");
+		 System.out.println("\n\n\n");
+		 System.out.println("\\n\n");
+	 }
+}	  	 
+		 
