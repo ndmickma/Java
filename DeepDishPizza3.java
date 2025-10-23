c765f7aaf512e24a438dbb3ac9d5f2a3dea30fbd
Sanvitti <86451908+ndmickma@users.noreply.github.com>
2025-10-23 11:35:29 -0700

    Add files via upload

---
+++ DeepDishPizza3.java
@@ -0,0 +1,21 @@
+//Sanvitti Shah
+//Per 2.
+//10-21-35
+//DeepDishPizza.java
+/*
+ * 
+ * 
+ */
+ 
+ public class DeepDishPizza3 extends Pizza3
+ {
+	 protected String toppingDeepDish, name;
+	 public DeepDishPizza3(String toppingDeepDishIn, String nameIn)
+	 {
+		 super(toppingDeepDishIn,nameIn);
+		 toppingDeepDish = toppingDeepDishIn;
+		 name = nameIn;
+	 }
+	 
+	 
+ }
