36a9b5438fa39025acdf01b1a5d327fba2dcdefc
Sanvitti <86451908+ndmickma@users.noreply.github.com>
2025-10-30 11:36:25 -0700

    Add files via upload

--- Postage.java
+++ Postage.java
@@ -17,32 +17,76 @@
 import java.util.Scanner;
 public class Postage
 {
-	private boolean validWeight;
-	private int weight;
 	private int selected;
+	private int weight;
+	private double cost;
 	
 	public Postage()
 	{
-		validWeight = true;
-		weight = 0;
 		selected = 0;
+		weight = 0;
+		cost = 0.0;
 	}
 	
 	public static void main(String [] args)
 	{
 		Postage mail = new Postage();
-		mail.sendThis;
+		mail.sendThis();
 	}
 	
-	public static void getData()
+	public void getData()
 	{
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Using the menu above, please enter the category of your postage -> ");
-		selected = keyboard.nextInt;
-		if(
+		selected = keyboard.nextInt();
+		if(selected == 1)
+		{
+			System.out.print("\nEnter the weight, in ounces, of your letter (a positive integer less than 2 million) -> ");
+			weight = keyboard.nextInt();
+		}
+		else if(selected == 3)
+		{
+			System.out.print("\nEnter the weight, in pounds, of your package (a positive integer less than 2 million) -> ");
+			weight = keyboard.nextInt(); 
+		}
+			
+				
 	}
-	public static double calculateCost()
+	public double calculateCost()
 	{
+		if(selected == 1)
+		{
+			if(weight<=0)
+			{
+				System.out.println("The weight you entered is not a valid input. Rerun the program and input a positive integer."); 
+			}
+			else
+			{
+				cost = 0.78 + (0.29*(weight-1));
+			}
+		}
+		else if(selected == 2)
+		{
+			cost = 0.61;
+		}	
+		
+		else if(selected == 3)
+		{
+			if(weight<=0)
+			{
+				System.out.println("The weight you entered is not a valid input. Rerun the program and input a positive integer."); 
+			}
+			else
+			{
+				cost = 4.47 + (0.72*(weight-1));
+			}
+			
+		}
+		else
+		{
+			System.out.println("Please enter a valid postage class!");
+		}
+
 	}
 	
 	public static void printMenu()
@@ -55,6 +99,22 @@ public class Postage
 	}
 	public static void printCost()
 	{
+		if(selected == 1)
+		{
+			System.out.printf("");
+		}
+		else if(selected == 2)
+		{
+			System.out.printf("");
+		}
+		else if(selected == 3)
+		{
+			System.out.printf("");
+		}
+		else
+		{
+			System.out.println("Have a nice day!");
+			System.out.print("\n\n\n");
 	}
 	public void sendThis() 
 	{
