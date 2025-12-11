489b119d948b4da2bc3aea5c1632259aa55d0b79
Sanvitti <86451908+ndmickma@users.noreply.github.com>
2025-12-11 14:11:00 -0800

    Add files via upload

--- Punctuation/Punctuation.java
+++ Punctuation/Punctuation.java
@@ -2,6 +2,15 @@
 //Per. 2
 //Punctuation.java
 /*
+ * Pseudocode:
+ * Write header
+ * Main
+ * 	make instance of class and use it to call search it
+ * searchIt(String str)
+ * 	call getPunctution and printWords
+ * 	return str
+ * getPunctuationWords
+ * 	
  * 
  * 
  */
@@ -10,27 +19,47 @@
  {
 	 public static void main(String [] args)
 	 {
-		 String text = new String("Hello. hey,how are you? HEY!");
+		 String text = new String("Hello's. \"hey,how are you?\" HEY!");
 		 Punctuation punc = new Punctuation();
 		 punc.searchIt(text);
 	 }
 	 public String searchIt(String str)
 	 {
-		 getPunctuationWords(str);
-		 printWords(str);
-		 return str;
+		 String abc = getPunctuationWords(str);;
+		 printWords(abc);
 	 }
 	 public String getPunctuationWords(String strIn1)
 	 {
-		 return checkForPunctuation(strIn1);
+		 String b = " ";
+		 String c = "";
+		 for(int j = 0; j<= strIn1.length(); j++)
+		 {
+			 char d = strIn1.charAt(j);
+			 if(d!= ' ')
+			 {
+				 b+=d;
+			 }
+			 else
+			 {
+				 if(checkForPunctuation(strIn1))
+				 {
+					 b+=(strIn1+"\n");
+				 }
+				 strIn1 = " ";
+			 } 
+		 }
+		 return b;
 	 }
 	 public String checkForPunctuation(String strIn2)
 	 {
+		 int counter = 0;
 		 for(int i = 0; i<=(strIn2.length())-1; i++)
 		 {
+			 counter++;
 			 if(strIn2.charAt(i) == '?' || strIn2.charAt(i) == '!' || strIn2.charAt(i) == ':' || strIn2.charAt(i) == '\"' || strIn2.charAt(i)== '\'' || strIn2.charAt(i) == '.' || strIn2.charAt(i)== ';')
 			 {
-				 strIn2.substring(strIn2.lastIndexOf(" ", i )+1, i+1); //THE LAST INDEX THING IS WRONG :(
+				 strIn2.substring(i-counter+1, i+1); 
+				 counter=0;
 			 } 
 		 }
 		 return strIn2;
---
+++ Punctuation/Punctuation2.java
@@ -0,0 +1,44 @@
+//Sanvitti Shah
+//Per. 2
+//Punctuation.java
+/*
+ * 
+ * 
+ */
+ 
+ public class Punctuation2
+ {
+	 public static void main(String [] args)
+	 {
+		 String text = new String("Hello. hey,how are you? HEY!");
+		 Punctuation2 punc2 = new Punctuation2();
+		 punc.searchIt(text);
+	 }
+	 public String searchIt(String str)
+	 {
+		 getPunctuationWords(str);
+		 printWords(str);
+		 return str;
+	 }
+	 public String getPunctuationWords(String strIn1)
+	 {
+		 return checkForPunctuation(strIn1);
+	 }
+	 public String checkForPunctuation(String strIn2)
+	 {
+		 for(int i = 0; i<=(strIn2.length())-1; i++)
+		 {
+			 if(strIn2.charAt(i) == '?' || strIn2.charAt(i) == '!' || strIn2.charAt(i) == ':' || strIn2.charAt(i) == '\"' || strIn2.charAt(i)== '\'' || strIn2.charAt(i) == '.' || strIn2.charAt(i)== ';')
+			 {
+				 strIn2.substring(strIn2.lastIndexOf(" ", i )+1, i+1); //THE LAST INDEX THING IS WRONG :(
+			 } 
+		 }
+		 return strIn2;
+	 }
+	 public void printWords(String strIn3)
+	 {
+		 System.out.println(strIn3);
+	 }
+	
+	 
+ }
