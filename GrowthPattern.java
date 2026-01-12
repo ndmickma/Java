65a52097919f4b1d9a529116126cc7d078b924a0
Sanvitti <86451908+ndmickma@users.noreply.github.com>
2026-01-12 10:08:04 -0800

    Add files via upload

---
+++ GrowthPattern.java
@@ -0,0 +1,23 @@
+//Sanvitti Shah
+//01-12-26
+//Per. 2
+//GrowthPattern.java
+/* Program description
+ * 
+ * 
+ * 
+ * 
+ * Working on
+ * arrays
+ * 
+ */
+ 
+public class GrowthPatterns
+{
+	public GrowthPatterns()
+	{
+	}
+	public static void main(String [] args)
+	{
+	}
+}
