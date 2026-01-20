7b5dfde0b51dcd1ac3a44c311d07373b1b751cba
Sanvitti <86451908+ndmickma@users.noreply.github.com>
2026-01-20 11:36:30 -0800

    Add files via upload

---
+++ GradeStats.java
@@ -0,0 +1,74 @@
+//Sanvitti Shah
+//Per. 2
+//01-15-26
+//GradeStats.java
+/*Program Description:
+ * 
+ * This program will calculate the statistics of grades.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+ 
+ /**Pseudocode
+  * FV array (int) studentScores
+  * main
+  * 	calls fake main
+  * calculateIt() fake main
+  * 	call other methods
+  * 	getInput()
+  * 	call findMin()
+  * 	call findMax()
+  * 	call findAverage()
+  * 	call findMedian()
+  * 	call findVeryBadScores
+  * 
+  * getInput()
+  * 	get user input and put it into studentScores
+  * 	and use Integer.parseInt() to convert the inputs into int
+  * findMin()
+  * 	make temp variable
+  * 	loop through studentScores and check for minimum 
+  * 	tempNumMin = studentScores[0];
+  * 	if(tempNumMin < studentScores[i])
+  * 		tempNumMin = studentScores[i]
+  * findMax()
+  * 	make temp variable
+  * 	loop through studentScores and check for maximum 
+  * 	tempNumMax = studentScores[0];
+  * 	if(tempNumMax > studentScores[i])
+  * 		tempNumMax = studentScores[i]
+  * findAverage()
+  * 	make 'average' variable
+  * 	loop through studentScores
+  * 	average += studentScores[i]
+  * 
+  * 	outside loop 
+  * 	average = average/studentScores.length';
+  * 
+  * findMedian()
+  * 	make temp variable 
+  * 	loop through studentScores[]
+  * 	for(
+  * 		
+  * 
+  * 
+  */
+
+import java.util.Scanner;
+
+public class GradeStats
+{
+	
+	public GradeStats()
+	{
+	}
+	public static void main(String [] args)
+	{
+	}
+}
