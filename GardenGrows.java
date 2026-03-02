51253db601babf39ada9391626d825e326375466
Sanvitti <86451908+ndmickma@users.noreply.github.com>
2026-03-02 10:06:04 -0800

    Add files via upload

---
+++ GardenGrows.java
@@ -0,0 +1,43 @@
+//Sanvitti Shah
+//Per.2
+//03-02-26
+//GardenGrows.java
+/*Program description
+ * This program will interact with the user (mouse/keyboard) 
+ * and when the user clicks certain things, ground is watered flowers grow
+ * 
+ * Working on:
+ * GUI
+ * Keyboard
+ * Mouse
+ * JFrame
+ * JPanel
+ * 
+ * 
+ */
+/**Pseudocode
+ * import everything
+ */
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class GardenGrows
+{
+	public static void main(String [] args)
+	{
+		GardenGrows gg = new GardenGrows();
+		gg.callEverything();
+	}
+	
+	public void runEverything()
+	{
+	}
+	
+}
