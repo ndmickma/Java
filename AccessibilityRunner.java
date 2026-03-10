aa8e613140008789e20859c083f67de78f63400a
Sanvitti <86451908+ndmickma@users.noreply.github.com>
2026-03-09 22:43:19 -0700

    Update AccessibilityRunner.java

--- AccessibilityRunner.java
+++ AccessibilityRunner.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Font;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -17,19 +18,20 @@ public class AccessibilityRunner
 	public AccessibilityRunner(){}
 	public static void main(String [] args)
 	{
-		AcessibilityRunner ar = new AccessibilityRunner();
+		AccessibilityRunner ar = new AccessibilityRunner();
 		ar.run();
 	}
 	
 	public void run()
 	{
-		JFrame frame = new JFrame(AccessibilityRunner);
+		JFrame frame = new JFrame("AccessibilityRunner");
 		frame.setSize(1000,800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(0,0);
-		frame.setResizeable(false);
+		frame.setResizable(false);
 		Accessibility pan = new Accessibility();
 		frame.getContentPane().add(pan);
+		frame.setVisible(true);
 	}
 }
 
@@ -67,6 +69,69 @@ class Accessibility extends JPanel implements MouseListener, KeyListener
     
     public void paintComponent(Graphics g)
     {
-		
+		super.paintComponent(g);
+		drawRectanglesWithLabels(g);
+		g.setColor(Color.BLACK);
+		Font font = new Font("Serif", Font.PLAIN,fontSize);
+		g.setFont(font);
+		writeDirections(g);
 	}
+	
+	public void keyPressed(KeyEvent evt)
+	{
+		int arrowCode = evt.getKeyCode();
+		if(arrowCode == KeyEvent.VK_UP)
+		{
+			if(fontSize+5>80)
+				fontSize = 80;
+			else
+				fontSize +=5;
+			repaint();
+		}
+		else if(arrowCode == KeyEvent.VK_DOWN)
+		{
+			if(fontSize-5<8)
+				fontSize = 8;
+			else
+				fontSize -=5;
+			repaint();
+		}
+	}
+	
+	public void keyTyped(KeyEvent evt){}
+	public void keyReleased(KeyEvent evt){}
+	
+	public void mousePressed(MouseEvent evt)
+	{
+		requestFocusInWindow();
+	}
+	
+	public void mouseClicked(MouseEvent evt)
+	{
+		int xMouse, yMouse;
+		xMouse = evt.getX();
+		yMouse = evt.getY();
+		
+		if((xMouse >=10 && xMouse<= 110) && (yMouse >= 10 && yMouse <=30))
+		{
+			if(colorParameterVar+3>255)
+				colorParameterVar = 255;
+			else
+				colorParameterVar +=3;
+			repaint();
+		}
+		
+		else if((xMouse >=115 && xMouse<= 215) && (yMouse >= 10 && yMouse <=30))
+		{
+			if(colorParameterVar-3 <34)
+				colorParameterVar=34;
+			else
+				colorParameterVar -=3;
+			repaint();
+		}
+	}
+	
+	public void mouseReleased(MouseEvent evt){}
+	public void mouseEntered(MouseEvent evt){}
+	public void mouseExited(MouseEvent evt){}
 }
